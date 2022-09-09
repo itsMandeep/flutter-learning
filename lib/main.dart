efc096d9308f41f9233ee6a6e0c6a5e3e65d5115
mandeepsingh-byjus <mandeep_whitehatjr>
2022-09-09 09:07:18 +0530

    initial app setup

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Material(
+        child: Center(
+          child: Text(
+            "data",
+            style: TextStyle(
+                backgroundColor: Color.fromARGB(255, 96, 52, 185),
+                color: Colors.white38),
+          ),
+        ),
+      ),
+    );
+  }
+}
